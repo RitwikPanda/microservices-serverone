4dd59eedbb9800224016fb9854d5234e4b17d214
HP <ritwikpanda99@gmail.com>
2023-03-05 01:31:30 +0530

    Initial commit

---
+++ src/main/java/com/user/microservicesserverone/MicroservicesServeroneApplication.java
@@ -0,0 +1,13 @@
+package com.user.microservicesserverone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesServeroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesServeroneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/microservicesserverone/MicroservicesServeroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.microservicesserverone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesServeroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
